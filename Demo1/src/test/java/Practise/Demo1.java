7cda56274a268ec5dbc965110a1e73540d3b8d97
kaust <kaust@KAUSTABH>
2023-08-27 19:07:04 +0530

    1 st push

---
+++ Demo1/src/test/java/Practise/Demo1.java
@@ -0,0 +1,10 @@
+package Practise;
+
+import org.testng.annotations.Test;
+
+@Test
+public class Demo1 {
+public void demo() {
+	System.out.println("Hi");
+}
+}
---
+++ Demo1/src/test/java/Practise/Demo2.java
@@ -0,0 +1,10 @@
+package Practise;
+
+import org.testng.annotations.Test;
+
+@Test
+public class Demo2 {
+public void demo() {
+	System.out.println("Hello");
+}
+}
